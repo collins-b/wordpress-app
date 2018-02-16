aad4c414afdec260439bf33aa72a2432f87de8a1
Collins <collins.wekesa@andela.com>
2018-02-16 22:23:16 +0300

    clean up code

--- tests/dummytest.php
+++ tests/dummytest.php
@@ -10,7 +10,6 @@ class RemoteConnectTest extends TestCase
 
   public function testConnectionIsValid()
   {
-    // test to ensure that the object from an fsockopen is valid
     $connObj = new RemoteConnect();
     $serverName = 'www.google.com';
     $this->assertTrue($connObj->connectToServer($serverName) !== false);
