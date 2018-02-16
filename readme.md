9653a84a79bd3284476478e19a7c75020ffa2b40
Collins <collins.wekesa@andela.com>
2018-02-16 20:43:04 +0300

    app/push wordpress app

---
+++ readme.md
@@ -0,0 +1,5 @@
+### Wordpress app to demonstrate LAMP deployment to GCP using K8s
+
+## Output 1
+
+Note: I'm not the one who built this app. I just picked a random project to so that I can demonstrate the skills that are being tested in this output
---
+++ tests/hellotest.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('hello.php');
+use PHPUnit\Framework\TestCase;
+
+class RemoteConnectTest extends TestCase
+{
+  public function setUp(){ }
+  public function tearDown(){ }
+
+  public function testConnectionIsValid()
+  {
+    // test to ensure that the object from an fsockopen is valid
+    $connObj = new RemoteConnect();
+    $serverName = '198.9.0.3';
+    $this->assertTrue($connObj->connectToServer($serverName) !== false);
+  }
+}
+?>
