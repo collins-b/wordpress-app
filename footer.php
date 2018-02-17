3890c644e8dacf72333a41788796a7872d3fedd8
Collins <collins.wekesa@andela.com>
2018-02-17 15:23:35 +0300

    test actually cd is working

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 				<!-- copyright -->
 				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. Buddy.Works :D
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>
 				</p>
 				<!-- /copyright -->
 
